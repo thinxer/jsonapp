c2d26c176f8cc0581edefac35a46e46fe7b43212
Jianfei Wang <me@thinxer.com>
2011-09-16 10:58:22 +0800

    autocomplete possible in Remote

--- jsonapp/client.py
+++ jsonapp/client.py
@@ -7,11 +7,23 @@ __all__ = ['Remote']
 class Remote(object):
     def __init__(self, endpoint):
         self._url = endpoint
+        self._methods = []
 
     def __getattr__(self, name):
-        return self._dumbmethod(name)
+        if name not in self.__methods__:
+            return None
+        return self._generalmethod(name)
 
-    def _request(self, name, params):
+    def __getitem__(self, name):
+        return getattr(self, name)
+
+    @property
+    def __methods__(self):
+        if not self._methods:
+            self._methods = map(str, self._request('rpc.listMethods'))
+        return self._methods
+
+    def _request(self, name, params = []):
         req = urllib2.Request(self._url, json.dumps({
             'method': name,
             'params': params
@@ -23,10 +35,11 @@ class Remote(object):
         else:
             return response['result']
 
-    def _dumbmethod(self, name):
-        def foo(*args):
+    def _generalmethod(self, name):
+        def generalmethod(*args):
             return self._request(name, args)
-        return foo
+        generalmethod.func_name = name
+        return generalmethod
 
 #r = Remote("http://localhost:8080/api")
 #r.hello("JsonApp")
