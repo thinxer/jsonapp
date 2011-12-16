52214137880882b783539344fa00a938b14cff1b
Jianfei Wang <me@thinxer.com>
2011-12-17 00:36:22 +0800

    client sub call

--- jsonapp/client.py
+++ jsonapp/client.py
@@ -8,34 +8,41 @@ import json
 __all__ = ['Remote']
 
 class Remote(object):
-    def __init__(self, endpoint):
-        self._url = endpoint
-        self._methods = []
+    def __init__(self, endpoint, basename=''):
+        self.__url = endpoint
+        self.__methods = []
+        self.__basename = basename
 
     def __getattr__(self, name):
-        if name not in self.__methods__:
+        if name in ['trait_names', '_getAttributeNames']:
             return None
-        return self._generalmethod(name)
+        if self.__basename and name:
+            newname = self.__basename + '.' + name
+        else:
+            newname = self.__basename or name
+        r = Remote(self.__url, newname)
+        r.__methods = [m[len(name)+1:] for m in self.__methods if m.startswith(name + '.')]
+        return r
 
     def __getitem__(self, name):
         return getattr(self, name)
 
     @property
     def __methods__(self):
-        if not self._methods:
-            self._methods = self._request('rpc.listMethods')
-        return self._methods
+        if not self.__methods:
+            self.__methods = self['rpc.listMethods']()
+        return self.__methods
 
     @property
     def __dict__(self):
         return {method: getattr(self, method) for method in self.__methods__}
 
-    def _request(self, name, params = []):
+    def __call__(self, *params):
         content = json.dumps({
-            'method': name,
+            'method': self.__basename,
             'params': params
             }).encode('utf8')
-        req = Request(self._url, content)
+        req = Request(self.__url, content)
         resp = urlopen(req)
         response = json.loads(resp.read().decode('utf8'))
         if 'error' in response:
@@ -43,12 +50,9 @@ class Remote(object):
         else:
             return response['result']
 
-    def _generalmethod(self, name):
-        def generalmethod(*args):
-            return self._request(name, args)
-        generalmethod.__name__ = name
-        return generalmethod
+    def __repr__(self):
+        return '<jsonapp.Remote: \'%s\' @ %s>' % (self.__basename, self.__url)
 
 if __name__ == '__main__':
-    r = Remote("http://localhost:8080")
+    r = Remote("http://localhost:8080/api")
     #r.hello("JsonApp")
